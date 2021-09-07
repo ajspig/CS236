d10e8df184dde084fcfac10b2b215f0761833eb6
ajspig <dragon@monstercode.com>
2021-09-07 13:10:24 -0600

    adding make file!

---
+++ Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/ColonAutomaton.cpp" "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/ColonAutomaton.cpp.o"
+  "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/ColonDashAutomaton.cpp" "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/ColonDashAutomaton.cpp.o"
+  "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/Lexer.cpp" "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Lexer.cpp.o"
+  "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/Token.cpp" "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Token.cpp.o"
+  "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/main.cpp" "/Users/abby/Documents/BYU/Fall 2021/CS 236/CS236/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
