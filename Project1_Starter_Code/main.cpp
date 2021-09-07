9d5ff4e16bc326bbb857678c26cf9c40ba75b8e6
ajspig <dragon@monstercode.com>
2021-09-07 14:05:50 -0600

    added lab pass of cases

--- Project1_Starter_Code/main.cpp
+++ Project1_Starter_Code/main.cpp
@@ -1,22 +1,24 @@
 #include "Lexer.h"
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[]) {
     //verify commandline arguments
     if (argc != 2) {
         std::cout << "usage: " << argv[0] << " input_file" << endl;
         return 1;
     }
     string fileName = argv[1];
-//    ifstream input(fileName);
-//    if (!input.is_open()) {
-//        cout << "File " << fileName << " could not be found or opened." << endl;
-//        return 1;
-//    }
+    ifstream input(fileName);
+    if (!input.is_open()) {
+        cout << "File " << fileName << " could not be found or opened." << endl;
+        return 1;
+    }
     //open an ifstream object
     //read the file into a string with get and peek
 
